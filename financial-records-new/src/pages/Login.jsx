65d6d2aba4a767411b53706edea17c0ba60a139a
slsl2 <kimminji7153@gmail.com>
2024-06-11 15:15:25 +0900

    style: 회원가입, 로그인 페이지 UI

---
+++ financial-records-new/src/pages/Login.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+import StyledContainer from "../styles/StyledContainer";
+import Button from "../components/atoms/Button";
+
+const Login = () => {
+  return (
+    <>
+      <LoginPage>
+        {" "}
+        <LoginForm>
+          <InputDiv>
+            <span>로그인 아이디</span>
+            <Input />
+          </InputDiv>
+          <InputDiv>
+            <span>비밀번호</span>
+            <Input />
+          </InputDiv>
+          <Button
+            width="100%"
+            backgroundColor="#999"
+            color="white"
+            margin="0 0 1.6rem 0"
+            contents="로그인"
+            type="button"
+          />
+          <Button
+            width="100%"
+            backgroundColor="#6c757d"
+            color="white"
+            contents="회원가입"
+            type="button"
+          />
+        </LoginForm>
+      </LoginPage>
+    </>
+  );
+};
+
+export default Login;
+
+const LoginPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoginForm = styled(StyledContainer).attrs({ as: "form" })`
+  font-size: 1.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  max-width: 480px;
+  margin: 0;
+  padding: 4rem;
+`;
+
+const InputDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 100%;
+`;
+const Input = styled.input`
+  font-size: 1.4rem;
+  margin: 0.8rem 0 1.6rem 0;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 5px;
+  outline: none;
+`;
--- financial-records-new/src/shared/Router.jsx
+++ financial-records-new/src/shared/Router.jsx
@@ -2,24 +2,28 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home.jsx";
 import Record from "../pages/Record.jsx";
+import Login from "../pages/Login.jsx";
+import SignUp from "../pages/SignUp.jsx";
 
 const Router = () => {
   const [selectedMonth, setSelectedMonth] = useState("");
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home
-                selectedMonth={selectedMonth}
-                setSelectedMonth={setSelectedMonth}
-              />
-            }
-          />
-          <Route path="/record/:id" element={<Record />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              selectedMonth={selectedMonth}
+              setSelectedMonth={setSelectedMonth}
+            />
+          }
+        />
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/record/:id" element={<Record />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
